4a70fd2ab982be51668618d5e4bbfea88f50b08b
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2017-03-10 16:06:45 +0300

    Add mult method

--- src/Cannon.cpp
+++ src/Cannon.cpp
@@ -1,4 +1,4 @@
-//#include <omp.h>
+#include <omp.h>
 #include <iostream>
 #include <ctime> 
 using namespace std;
@@ -24,6 +24,16 @@ int matrixPrint(double *&M, const int size) {
 	return 0;
 }
 
+int matrixMult(double *&A, double *&B, double *&C, int size) {
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++) {
+			C[idx(i, j, size)] = 0;
+			for (int k = 0; k < size; k++)
+				C[idx(i, j, size)] += A[idx(i, k, size)] * B[idx(k, j, size)];
+		}
+	return 0;
+}
+
 int main(int argc, char** argv) {
 	double *A = nullptr;
 	double *B = nullptr;
@@ -35,17 +45,22 @@ int main(int argc, char** argv) {
 	srand((unsigned int)time(NULL));
 	A = new double [size*size];
 	B = new double [size*size];
+	C = new double [size*size];
 	matrixGenerate(A, size);
 	matrixGenerate(B, size);
 	
-	matrixPrint(A, size);
-	matrixPrint(B, size);
+	//matrixPrint(A, size);
+	//matrixPrint(B, size);
 
-
-
-
+	double startTime = omp_get_wtime();
+	matrixMult(A, B, C, size);
+	double endTime = omp_get_wtime();
+	
+	//matrixPrint(C, size);
+	cout << "Time: " << endTime - startTime << endl;
 
 	delete []A;
 	delete []B;
+	delete []C;
 	return 0;
 }
