6b0eacf9677e651356f73a556d4d180df3f87d20
Maxim Doronin <doro156@me.com>
2017-03-15 18:35:47 +0300

    Add shifts. Not tested

--- src/Cannon.cpp
+++ src/Cannon.cpp
@@ -34,6 +34,50 @@ int matrixMult(double *&A, double *&B, double *&C, int size) {
 	return 0;
 }
 
+int shiftLeft(double *&M, int size, const int blockSize, const int init) {
+	double *aux = nullptr;
+	aux = new double[size];
+	int step = blockSize;
+
+	for (int k = 0, int s = 0; k < size; k += blockSize, s++) {
+		for (int i = k; i < (k + blockSize); i++) {
+			if (init > 0)
+				step = s * blockSize;
+			for (int j = 0; j < size; j++)
+				aux[j] = M[idx(i, ((j + step) % size), size)];
+			for (int j = 0; j < size; j++)
+				M[idx(i, j, size)] = aux[j];
+		}
+	}
+}
+
+int shiftUP(double *&M, int size, const int blockSize, const int init) {
+	double *aux = nullptr;
+	aux = new double[size];
+	int step = blockSize;
+
+	for (int k = 0, int s = 0; k < size; k += blockSize, s++) {
+		for (int i = k; i < (k + blockSize); i++) {
+			if (init > 0)
+				step = s * blockSize;
+			for (int j = 0; j < size; j++)
+				aux[j] = M[idx(((j + step) % size), i, size)];
+			for (int j = 0; j < size; j++)
+				M[idx(j, i, size)] = aux[j];
+		}
+	}
+}
+
+
+
+int cannonPar(double *&A, double *&B, double *&C, int xblock) {
+	for (int i = 0; i < xblock; i++) {
+		multProcessPar();
+		shiftLeft();
+		shiftUP();		
+	}
+}
+
 int main(int argc, char** argv) {
 	double *A = nullptr;
 	double *B = nullptr;
