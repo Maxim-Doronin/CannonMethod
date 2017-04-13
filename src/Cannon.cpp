70369da1d02b4879481fdc4c2d36dc87d6689863
Maxim Doronin <doro156@me.com>
2017-04-13 13:02:59 +0300

    Add tbb parallelization

--- src/Cannon.cpp
+++ src/Cannon.cpp
@@ -1,26 +1,56 @@
 #include <omp.h>
 #include <iostream>
 #include <ctime> 
+#include "tbb/tbb.h"
+#include "tbb/task_scheduler_init.h"
 using namespace std;
+using namespace tbb;
 
 inline int idx(int i, int j, int size) {
-	return i*size + j;
+    return i*size + j;
 }
+
+int matrixPrint(double *&M, const int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+            cout << M[idx(i, j, size)] << "\t";
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
+
+class CannonTask {
+private:
+    int rowStartIdx;
+    int colStartIdx;
+    int size;
+    double *A;
+    double *B;
+    double *C;
+    int xSize;
+public:
+    CannonTask(double *_A, double *_B, double *_C,
+        int _rowStartIdx, int _colStartIdx, int _size, int _xSize) :
+        A(_A), B(_B), C(_C), rowStartIdx(_rowStartIdx), colStartIdx(_colStartIdx),
+        size(_size), xSize(_xSize) {};
+    void operator()() const {
+        int blockSize = size / xSize;
+        for (int x = 0; x < xSize; x++) {
+            for (int i = rowStartIdx * blockSize; i < (rowStartIdx + 1)*blockSize; i++)
+                for (int j = colStartIdx * blockSize; j < (colStartIdx + 1)*blockSize; j++)
+                    for (int k = x * blockSize; k < (x + 1)*blockSize; k++) {
+                        C[idx(i, j, size)] += A[idx(i, k, size)] * B[idx(k, j, size)];
+                    }
+       }
+        
+    }
+};
 
 int matrixGenerate(double *&M, const int size) {
 	for (int i = 0; i < size; i++)
 		for (int j = 0; j < size; j++)
 			M[idx(i, j, size)] = rand() % 100 - 50;
-	return 0;
-}
-
-int matrixPrint(double *&M, const int size) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++)
-			cout << M[idx(i, j, size)] << "\t";
-		cout << endl;
-	}
-	cout << endl;
 	return 0;
 }
 
@@ -34,19 +64,19 @@ int matrixMult(double *&A, double *&B, double *&C, int size) {
 	return 0;
 }
 
-int multProcessPar(double *&A, double *&B, double *&C, const int size, const int numthreads, int i) {
-	int blockSize = size / numthreads;
+int multProcessPar(double *&A, double *&B, double *&C, const int size, const int xSize, int i) {
+	int blockSize = size / xSize;
 	int l, m, r, c, k, rbegin, rend, cbegin, cend, idThread;
 	double *sa = nullptr;
 	double *sb = nullptr;
 	double *sc = nullptr;
-#pragma omp parallel default(none) private(l, m, r, c, k, rbegin, rend, cbegin, cend, idThread, sa, sb, sc) shared(A, B, C, size, blockSize, numthreads, i) num_threads(numthreads * numthreads)
+#pragma omp parallel default(none) private(l, m, r, c, k, rbegin, rend, cbegin, cend, idThread, sa, sb, sc) shared(A, B, C, size, blockSize, xSize, i) num_threads(xSize * xSize)
 	{
 		idThread = omp_get_thread_num();
-		rbegin = (idThread / numthreads) * blockSize;
+		rbegin = (idThread / xSize) * blockSize;
 		rend = rbegin + blockSize;
 
-		cbegin = (idThread % numthreads) * blockSize;
+		cbegin = (idThread % xSize) * blockSize;
 		cend = cbegin + blockSize;
 
 		sa = new double[blockSize * blockSize];
@@ -55,8 +85,8 @@ int multProcessPar(double *&A, double *&B, double *&C, const int size, const int
 
 		for (r = rbegin, l = 0; r < rend; r++, l++) {
 			for (c = cbegin, m = 0; c < cend; c++, m++) {
-				sa[idx(l, m, blockSize)] = A[idx(r, (c + i*blockSize + (idThread / numthreads) * blockSize) % size, size)];
-				sb[idx(l, m, blockSize)] = B[idx((r + i*blockSize + (idThread % numthreads) * blockSize) % size, c, size)];
+				sa[idx(l, m, blockSize)] = A[idx(r, (c + i*blockSize + (idThread / xSize) * blockSize) % size, size)];
+				sb[idx(l, m, blockSize)] = B[idx((r + i*blockSize + (idThread % xSize) * blockSize) % size, c, size)];
 			}
 		}
 
@@ -114,17 +144,17 @@ int multProcessPar(double *&A, double *&B, double *&C, const int size, const int
 	return 0;
 }
 
-int multProcessCon(double *&A, double *&B, double *&C, int size, int numthreads, int i) {
-	int blockSize = size / numthreads;
+int multProcessCon(double *&A, double *&B, double *&C, int size, int xSize, int i) {
+	int blockSize = size / xSize;
 	int l, m, r, c, k, rbegin, rend, cbegin, cend, idThread;
 	double *sa = nullptr;
 	double *sb = nullptr;
 	double *sc = nullptr;
-	for (idThread = 0; idThread < numthreads * numthreads; idThread++) {
-		rbegin = (idThread / numthreads) * blockSize;
+	for (idThread = 0; idThread < xSize * xSize; idThread++) {
+		rbegin = (idThread / xSize) * blockSize;
 		rend = rbegin + blockSize;
 
-		cbegin = (idThread % numthreads) * blockSize;
+		cbegin = (idThread % xSize) * blockSize;
 		cend = cbegin + blockSize;
 
 		sa = new double[blockSize * blockSize];
@@ -133,8 +163,8 @@ int multProcessCon(double *&A, double *&B, double *&C, int size, int numthreads,
 
 		for (r = rbegin, l = 0; r < rend; r++, l++) {
 			for (c = cbegin, m = 0; c < cend; c++, m++) {
-				sa[idx(l, m, blockSize)] = A[idx(r, (c + i*blockSize + (idThread / numthreads) * blockSize) % size, size)];
-				sb[idx(l, m, blockSize)] = B[idx((r + i*blockSize + (idThread % numthreads) * blockSize) % size, c, size)];
+				sa[idx(l, m, blockSize)] = A[idx(r, (c + i*blockSize + (idThread / xSize) * blockSize) % size, size)];
+				sb[idx(l, m, blockSize)] = B[idx((r + i*blockSize + (idThread % xSize) * blockSize) % size, c, size)];
 			}
 		}
 
@@ -152,20 +182,29 @@ int multProcessCon(double *&A, double *&B, double *&C, int size, int numthreads,
 	return 0;
 }
 
-int cannonPar(double *&A, double *&B, double *&C, int size, int numthreads) {
-	int blockSize = size / numthreads;
-	for (int i = 0; i < numthreads; i++) {
-		multProcessPar(A, B, C, size, numthreads, i);
+int cannonPar(double *&A, double *&B, double *&C, int size, int xSize) {
+	int blockSize = size / xSize;
+	for (int i = 0; i < xSize; i++) {
+		multProcessPar(A, B, C, size, xSize, i);
 	}
 	return 0;
 }
 
-int cannonCon(double *&A, double *&B, double *&C, int size, int numthreads) {
-	int blockSize = size / numthreads;
-	for (int i = 0; i < numthreads; i++) {
-		multProcessCon(A, B, C, size, numthreads, i);
+int cannonCon(double *&A, double *&B, double *&C, int size, int xSize) {
+	int blockSize = size / xSize;
+	for (int i = 0; i < xSize; i++) {
+		multProcessCon(A, B, C, size, xSize, i);
 	}
 	return 0;
+}
+
+int cannonTBB(double *&A, double *&B, double *&C, int size, int xSize) {
+    task_group taskGroup;
+    for (int i = 0; i < xSize; i++)
+        for (int j = 0; j < xSize; j++)
+            taskGroup.run(CannonTask(A, B, C, i, j, size, xSize));
+    taskGroup.wait();
+    return 0;
 }
 
 double matrixCond(double *&A, double *&B, int size) {
@@ -182,20 +221,22 @@ int main(int argc, char** argv) {
 	double *C = nullptr;
 	double *C1 = nullptr;
 	double *C2 = nullptr;
+    double *C3 = nullptr;
 	int size = 0;
 	int numthreads = 1;
 	size = atoi(argv[1]);
 	if (argc == 3)
 		numthreads = atoi(argv[2]);
-	if (size%numthreads != 0) {
+	if (size % (int)sqrt((int)numthreads) != 0) {
 		cout << "Error!" << endl;
 		return -1;
 	}
+    int xSize = (int)sqrt((int)numthreads);
 
 	cout << endl;
 	cout << "---------Cannon algorithm for matrix multiplication-----------" << endl << endl;
 	cout << "Matrix size: " << size << endl;
-	cout << "Number of blocks: " << numthreads * numthreads << endl;
+	cout << "Number of blocks: " << xSize * xSize << endl;
 	
 	srand((unsigned int)time(NULL));
 	A = new double [size*size];
@@ -203,10 +244,12 @@ int main(int argc, char** argv) {
 	C = new double [size*size];
 	C1 = new double[size*size];
 	C2 = new double[size*size];
+    C3 = new double[size*size];
 	for (int i = 0; i < size * size; i++) {
 		C[i] = 0;
 		C1[i] = 0;
 		C2[i] = 0;
+        C3[i] = 0;
 	}
 	matrixGenerate(A, size);
 	matrixGenerate(B, size);
@@ -217,17 +260,25 @@ int main(int argc, char** argv) {
 	cout << "Trivial mult time: " << endTimeTM - startTimeTM << endl;
 
 	double startTimeCC = omp_get_wtime();
-	cannonCon(A, B, C2, size, numthreads);
+	cannonCon(A, B, C2, size, xSize);
 	double endTimeCC = omp_get_wtime();
 	cout << "Cannon consistent time: " << endTimeCC - startTimeCC << endl;
-
+    
 	double startTimeCP = omp_get_wtime();
-	cannonPar(A, B, C1, size, numthreads);
+	cannonPar(A, B, C1, size, xSize);
 	double endTimeCP = omp_get_wtime();
-	cout << "Cannon parallel time: " << endTimeCP - startTimeCP << endl;
+	cout << "OpenMP parallel time: " << endTimeCP - startTimeCP << endl;
+    
+    task_scheduler_init init();
+    double startTimeTBB = omp_get_wtime();
+    cannonTBB(A, B, C3, size, xSize);
+    double endTimeTBB = omp_get_wtime();
+    cout << "TBB parallel time   : " << endTimeTBB - startTimeTBB << endl;
 
-	cout << "Boost: " << (endTimeCC - startTimeCC) / (endTimeCP - startTimeCP) << endl;
-	cout << "Error: " << matrixCond(C1, C2, size) << endl;
+	cout << "Boost OpenMP: " << (endTimeCC - startTimeCC) / (endTimeCP - startTimeCP) << endl;
+    cout << "Boost TBB   : " << (endTimeCC - startTimeCC) / (endTimeTBB - startTimeTBB) << endl;
+	cout << "Error OpenMP: " << matrixCond(C1, C2, size) << endl;
+    cout << "Error TBB   : " << matrixCond(C3, C2, size) << endl;
 
 	delete []A;
 	delete []B;
